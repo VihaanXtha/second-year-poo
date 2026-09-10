430fd9c38eabe4083c8b3d8230ed42be0a0965c6
Vihaan Shrestha <sthaushab4@gmail.com>
2026-09-11 04:25:34 +0545

    ok

--- backend/tmp/test_env.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-echo "DB_HOST from env: " . getenv('DB_HOST') . "\n";
-echo "DB_HOST from $_ENV: " . ($_ENV['DB_HOST'] ?? 'not set') . "\n";
--- backend/tmp/test_shell_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export DB_HOST=mysql
-php -r 'require "vendor/autoload.php"; $app = require_once "bootstrap/app.php"; echo getenv("DB_HOST") . PHP_EOL;'
